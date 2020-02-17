e25429e7c0120d6dc8d93f86f41b2d46238b0b6a
Ramanpreet Singh <singh.ramanpreet09@gmail.com>
2020-02-17 19:55:11 +0100

    batch running tweaks

--- plotting/run_batch.sh
+++ plotting/run_batch.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-./make_mc_data_hists.py --region cuts00 --lepton m
+run_list=("signal_tight_W")
+run_list+=("signal_tight_01_W" "signal_tight_02_W" "signal_tight_03_W" "signal_tight_04_W" "signal_tight_05_W")
 
-./make_mc_data_hists.py --region cuts0_W --lepton m
-
-./make_mc_data_hists.py --region cuts1_W --lepton m
-
-./make_mc_data_hists.py --region cuts2_W --lepton m
-
-./make_mc_data_hists.py --region cuts3_W --lepton m
+for region in ${run_list[*]}; do
+    echo ${region}
+    ./make_mc_data_hists.py --region ${region} --lepton m
+    ./make_mc_data_hists.py --region ${region} --lepton e
+    hadd ${region}.root ${region}_m.root ${region}_e.root
+done
