8ef047d06e953a6fc7cdd8d092b7753d686f8aa0
Ramanpreet Singh <singh.ramanpreet09@gmail.com>
2020-09-17 12:10:48 -0500

    faster tar, clean setup env

--- prepare/make_tar.sh
+++ prepare/make_tar.sh
@@ -5,11 +5,10 @@ rm -v setup.tar.gz
 tar -zcf setup.tar.gz -C ../../ WVAnalysis/ \
     --exclude="setup.tar.gz" \
     --exclude="*.root" \
-    --exclude="*.ipynb*" \
     --exclude="*.pdf" \
     --exclude="*.png" \
     --exclude="*.err" \
     --exclude="*.out" \
     --exclude="*.log" \
-    --exclude-caches-all --exclude-vcs
+    --exclude-caches-all
 
