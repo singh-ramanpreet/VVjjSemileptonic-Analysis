239bb032a6fdfca8568ae0c5d5f0104542e86519
Ramanpreet Singh <singh.ramanpreet09@gmail.com>
2021-01-06 12:01:04 -0600

    improve hist condor submission

--- plotting/run_make_hists.sh
+++ plotting/run_make_hists.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 export PYTHONUNBUFFERED=true
-# ./run_make_hists.sh {local, condor} <region> <year> <base-dir> <in-dir> <threads> <hist out name>
+# ./run_make_hists.sh {local, .tar.gz file} <region> <year> <base-dir> <in-dir> <threads> <hist out name>
 
-if [[ ${1} == "condor" ]]; then
-    tar -xzf setup.tar.gz
-    cd WVAnalysis
-    source setup_env/setup.sh
-    cd plotting
-fi
-
+tar_file=${1}
 region=${2//./ --regions }
 year=${3}
 base_dir=${4}
@@ -16,18 +10,26 @@ in_dir=${5}
 threads=${6}
 output=${7}
 
-./make_hists.py \
-    --base-dir ${base_dir} \
-    --in-dir ${in_dir} \
-    --year ${year} \
-    --regions ${region} \
-    --output ${output} \
-    --threads ${threads}
+if [[ ${tar_file} == *".tar.gz" ]]
+then
+  mkdir -p Analysis
+  tar -xzf setup.tar.gz --strip 1 --directory Analysis
+  cd Analysis
+  source setup/setup.sh
+  cd plotting
+fi
 
-if [[ ${1} == "condor" ]]; then
-    ls -al
-    mv -vf ${output} ../../${output}
-    cd ../../
-    ls -al
-    rm -vf *docker*
+./make_hists.py \
+  --base-dir ${base_dir} \
+  --in-dir ${in_dir} \
+  --year ${year} \
+  --regions ${region} \
+  --output ${output} \
+  --threads ${threads}
+
+if [[ ${tar_file} == *".tar.gz" ]]
+then
+  mv -vf ${output} ../../${output}
+  cd ../../
+  rm -vf *docker*
 fi
